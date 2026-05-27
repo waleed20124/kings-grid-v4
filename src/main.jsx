96c2696e27fa88507cc12305bcae7290484bd616
waleed20124 <136029331+waleed20124@users.noreply.github.com>
2026-05-28 02:40:58 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ import {
 } from "./rules/quoridor.js";
 import "./styles.css";
 
-const socket = io("https://kings-grid-v4.onrender.com");
+const socket = io("https://kings-grid-v4-1.onrender.com");
 
 function useGameAudio(enabled) {
   const contextRef = useRef(null);
