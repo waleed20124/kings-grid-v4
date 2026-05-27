ebec6496ee60b0471c71f737c0ae331b826fd26a
waleed20124 <136029331+waleed20124@users.noreply.github.com>
2026-05-28 02:50:17 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 const httpServer = createHttpServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "https://sparkly-faun-b35556.netlify.app",
+    origin: "*",
     methods: ["GET", "POST"]
   }
 });
